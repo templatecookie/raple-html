57734be80a3f55146073fe1112199cca76ce4b21
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2021-12-11 16:15:36 +0600

    payment card one add

--- src/js/main.js
+++ src/js/main.js
@@ -156,7 +156,22 @@ $('.opener_sidebar').on('click', function (e) {
     }
   });
 
-  
+  // copyToClipboard
+
+const copy   = document.getElementById("copyButton");
+const selection = window.getSelection();
+const range = document.createRange();
+const textToCopy = document.getElementById("textToCopy")
+
+copy.addEventListener('click', function(e) {
+    range.selectNodeContents(textToCopy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const successful = document.execCommand('copy');
+    if(successful){
+    }
+    window.getSelection().removeAllRanges()
+});
 
   //hide & show
 })(jQuery);
