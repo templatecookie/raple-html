bbc5dc797b3c280736325c2e13878cc1d66b252b
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2021-12-18 12:20:22 +0600

    pricing page add

--- src/js/main.js
+++ src/js/main.js
@@ -722,5 +722,41 @@ if (document.getElementById('bar-chart1')) {
         .text($(this).text());
     });
 
+    /*--------------------------------------------------------------
+PRICING TABLE JS INIT
+------------------------------------------------------------*/
+    // Table BTN Trigger
+    $("#l5-pricing-btn .toggle-btn").on("click", function (e) {
+      console.log($(e.target).parent().parent().hasClass("monthly-active"));
+      $(e.target).toggleClass("clicked");
+      if ($(e.target).parent().parent().hasClass("monthly-active")) {
+        $(e.target)
+          .parent()
+          .parent()
+          .removeClass("monthly-active")
+          .addClass("yearly-active");
+      } else {
+        $(e.target)
+          .parent()
+          .parent()
+          .removeClass("yearly-active")
+          .addClass("monthly-active");
+      }
+    });
+
+    $("[data-pricing-trigger]").on("click", function (e) {
+      $(e.target).addClass("active").siblings().removeClass("active");
+      var target = $(e.target).attr("data-target");
+      console.log($(target).attr("data-value-active") == "monthly");
+      if ($(target).attr("data-value-active") == "monthly") {
+        $(target).attr("data-value-active", "yearly");
+      } else {
+        $(target).attr("data-value-active", "monthly");
+      }
+    });
+
+
+
+
 
 })(jQuery);
