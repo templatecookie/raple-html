46b8363acc1f30b3d58e6c17ed2de0c4ef964299
SheikhRashed <devrashed06@gmail.com>
2021-12-27 09:28:32 +0600

    content page on progress

--- src/js/main.js
+++ src/js/main.js
@@ -942,39 +942,34 @@ if (document.getElementById('bar-chart1')) {
         extraKeys: {"Ctrl-Space": "autocomplete"},
         lineNumbers: true,
         keyMap:"sublime",
-        tabSize:4,
+        tabSize:2,
     };
 
     // initialize editor
-    var editor = CodeMirror.fromTextArea(document.getElementById('editor'),config);
+    var editor = CodeMirror.fromTextArea(document.querySelector('.editor'),config);
+
     editor.setOption("theme", "material-ocean");
 
-	function loadHtml(html) {
-		const document_pattern = /( )*?document\./i;
-		let finalHtml = html.replace(document_pattern, "document.getElementById('result').contentWindow.document.");
-		$('#result').contents().find('html').html(finalHtml);
-	}
+	// function loadHtml(html) {
+	// 	const document_pattern = /( )*?document\./i;
+	// 	let finalHtml = html.replace(document_pattern, "document.getElementById('result').contentWindow.document.");
+	// 	$('#result').contents().find('html').html(finalHtml);
+	// }
 
-	loadHtml($('#editor').val());
+	// loadHtml($('.editor').val());
 
     editor.on('change',function(cMirror){
 
         if (myTimeoutId!==null) {
             clearTimeout(myTimeoutId);
         }
+
         myTimeoutId = setTimeout(function() {
-
-                try{
-
+                try {
                     loadHtml(cMirror.getValue());
-
-                }catch(err){
-
-                    console.log('err:'+err);
-
+                } catch(err){
+                    console.log('err:'+ err);
                 }
-
-
             }, 1000);
 
     });
